fd09f60e4abfe64cff3e0d462a8cce93e44709ee
shrutiverma97 <shruti@dream11.com>
2020-10-21 00:03:23 +0530

    feat(App.js): Move golf ball to the right

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,42 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,//variable needed to be changed
-            posi : 0,
+            posi: 0,
             ballPosition: { left: "0px" }
         };
         this.renderChoice = this.renderChoice.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
+        this.handleKey = this.handleKey.bind(this)
+    } 
+ 
     //call back function
     buttonClickHandler() {
-   
-   }
+        this.setState({ renderBall: true })
+    }
     renderChoice() {
-		if(this.state.renderBall){return <div className="ball" style={this.state.ballPosition}></div>}
-		 else   return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+        if (this.state.renderBall) { return <div className="ball" style={this.state.ballPosition}></div> }
+        else return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+    }
+
+    handleKey(e) {
+        e.preventDefault()
+        if (e.code === 'ArrowRight') {
+            let position = this.state.posi + 5
+            this.setState(prevState => ({
+                ballPosition: {
+                    ...prevState.ballPosition,
+                    left: `${position}px`
+                },
+                posi: position
+            })
+            )
+        }
+
     }
 
     //bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener("keydown", this.handleKey)
     }
 
     render() {
